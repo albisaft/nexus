761fa8eb90cf565cb54cdfd2c0d5cb8bb10c0a9e
albisaft <69861682+albisaft@users.noreply.github.com>
2024-04-24 18:58:35 +0200

    240415 AL 0.6
    Angriffe vom Läufer sind weniger wichtig.

--- main Hybrid.cpp
+++ main Hybrid.cpp
@@ -233,7 +233,7 @@ beginning:
             // wichtige Initkommandos - wo man antworten muss
 
             if (command == "uci") {
-                cout << "id name NEXUS 240326 EK 0.5\n";  // 750250 6000
+                cout << "id name NEXUS 240415 AL 0.6\n";  // 750250 6000
                 cout << "id author Albrecht Fiebiger & Stefan Werner\n";
                 cout << "uciok\n"; }
 
