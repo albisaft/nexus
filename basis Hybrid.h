b48a0a18d4402f6e32d0aa20934a2e5bdc3af1e2
albisaft <69861682+albisaft@users.noreply.github.com>
2024-05-12 21:09:27 +0200

    240424 AI 3
    Meine Angriffe zählen weniger

--- basis Hybrid.h
+++ basis Hybrid.h
@@ -53,7 +53,7 @@ int figurenwert = 0;
 
 int    KooIch    = 365;  //???
 int    KooEr     = 50;    //???
-double AttackIch = 5;
+double AttackIch = 3;
 double AttackEr  = 3;
 int    DefIch1   = 80;
 int    DefIch2   = 80;
